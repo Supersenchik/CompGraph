f67c8d94897e80a089094d3f958a941b4756048d
Supersenchik <31993@mail.ru>
2015-10-27 15:02:23 +0500

    Вращающийся куб

--- CompGraphProj/code/src/PixelShader.hlsl
+++ CompGraphProj/code/src/PixelShader.hlsl
@@ -1,8 +1,8 @@
-float4 main( float4 pos : SV_POSITION ) : SV_TARGET
-{
-	float fLimiter = 500.0f;
-	float dist = pos.x*pos.x + pos.y*pos.y;
-	dist = (dist % fLimiter) / fLimiter;
-	return float4(dist, 0.0f, dist, 1.0f);
-	//return float4(1.0f, 1.0f, 0.0f, 1.0f);
+struct VS_OUTPUT {
+    float4 pos : SV_POSITION;
+    float4 color : COLOR0;
+};
+
+float4 main(VS_OUTPUT input) : SV_Target {
+	return input.color;
 }
--- CompGraphProj/code/src/VertexShader.hlsl
+++ CompGraphProj/code/src/VertexShader.hlsl
@@ -1,4 +1,16 @@
-float4 main( float4 pos : POSITION ) : SV_POSITION
-{
-	return float4(pos.x-0.5, pos.y, pos.z, pos.w);
+struct VS_OUTPUT {
+    float4 pos : SV_POSITION;
+    float4 color : COLOR0;
+};
+
+cbuffer ConstantBuffer : register(b0) {
+	matrix model;
+}
+
+VS_OUTPUT main(float4 pos : POSITION, float4 color : COLOR) {
+	VS_OUTPUT output = (VS_OUTPUT)0;
+	output.pos = mul(pos, model);
+	output.pos.x = output.pos.x / 1.6f;
+	output.color = color;
+	return output;
 }
